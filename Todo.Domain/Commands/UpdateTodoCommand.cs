62c37cabbf73f362bb0c8e067919f808ca76e5d4
Caio Cesar <caiocsmeneghelli@outlook.com>
2021-11-04 17:33:31 -0300

    Adicionando mais commands.

--- Todo.Domain/Commands/UpdateTodoCommand.cs
+++ Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -5,27 +5,26 @@ using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class CreateTodoCommand : Notifiable, ICommand
+    public class UpdateTodoCommand : Notifiable, ICommand
     {
-        public CreateTodoCommand() { }
-        public CreateTodoCommand(string title, string user, DateTime date)
+        public UpdateTodoCommand() { }
+        public UpdateTodoCommand(Guid id, string title, string user)
         {
+            Id = id;
             Title = title;
             User = user;
-            Date = date;
         }
 
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string User { get; set; }
-        public DateTime Date { get; set; }
-
         public void Validate()
         {
             AddNotifications(
                 new Contract()
-                    .Requires()
-                    .HasMinLen(Title, 3, "Title", "Titulo invalido.")
-                    .HasMinLen(User, 6, "User", "Usuario invalido.")
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor essa tarefa!")
+                .HasMinLen(User, 6, "User", "Usuario invalido.")
             );
         }
     }
