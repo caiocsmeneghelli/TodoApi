e0c88beef8caec5c3fca1d91c4b18cb91746010a
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-02-07 22:10:01 -0300

    Commit com acerto mais ou menos.

--- Todo.Domain.Infra/Contexts/TodoContext.cs
+++ Todo.Domain.Infra/Contexts/TodoContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Todo.Domain.Entities;
 
 namespace Todo.Domain.Infra.Contexts
@@ -11,11 +12,11 @@ namespace Todo.Domain.Infra.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TodoItem>().ToTable("Todo");
+            // modelBuilder.Entity<TodoItem>().ToTable("Todo");
             modelBuilder.Entity<TodoItem>().Property(x => x.Id);
-            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
-            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
-            modelBuilder.Entity<TodoItem>().Property(x => x.Done).HasColumnType("bit");
+            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120); //.HasColumnType("varchar(120)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(160); //.HasColumnType("varchar(160)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Done); //.HasColumnType("bit");
             modelBuilder.Entity<TodoItem>().Property(x => x.Date);
             modelBuilder.Entity<TodoItem>().HasIndex(b => b.User);
         }
